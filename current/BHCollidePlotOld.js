908d0ac58cf28e4d9c73a7bb0c72b45cc29a469b
root <root@lune.wustl.edu>
2021-07-13 11:25:27 -0500

    first working image

---
+++ current/BHCollidePlotOld.js
@@ -0,0 +1,58 @@
+//Ploting javascript file
+function go(){
+    
+  var data = {
+  x:[],
+  y:[],
+  type: 'scatter'
+  }
+ 
+    
+var fx = document.getElementById("fx").value;
+
+var min = _getSize(0);
+var i0max = _getSize(1)-min;
+var i1max = _getSize(2)-min;
+var i2max = _getSize(3)-min;
+    
+var layout= {
+    xaxis: {
+      range: [-1.4,1.4]
+    },
+    yaxis: {
+      range: [-1.4,1.4]
+    }
+}
+
+var i=0;
+    
+for(let i2=min;i2<i2max;i2++){
+    for(let i1=min;i1<i1max;i1++){
+        for(let i0=min;i0<i0max;i0++){
+            let idx = _getIDX3S(i0,i1,i2);
+            var xCart = [];
+            xCart[0] = _getCart(i0,i1,i2,1);
+            xCart[1] = _getCart(i0,i1,i2,2);
+            console.log(xCart[0], xCart[1], _getIDX4ptS(fx,idx), _getCF(_getIDX4ptS(fx,idx)));
+           
+            /*
+            data.z[0][i] = xCart[0];
+            data.z[1][i] = xCart[1];
+            data.z[2][i] = _getCF(_getIDX4ptS(fx,idx));
+            */
+            
+                data.x[i]=xCart[0];
+                data.y[i]=xCart[1];
+            i++;
+        }
+    }
+}
+    
+    Plotly.newPlot("graph", [data], layout);
+};
+
+function runSim(){
+    console.log("Running");
+   _runsim();
+    console.log("Done");
+};
--- current/jim/BHCollidePlot.js
+++ current/jim/BHCollidePlot.js
@@ -1,14 +1,27 @@
 //Ploting javascript file
-var verbose = false;
+var count=0;
+
+var verbose = true;
 
 function graphSlice(){
+/*
+    if (count==0){
         
     var data = {
         x:[],
         y:[],
-        z:[]
+        type: 'scatter'
     }
-  
+
+    Plotly.newPlot("graph", [data]);
+    count++;
+      
+    var update = {
+        x: [data.x],
+        y: [data.y]
+    }
+    
+ */   
     // The main loop
     
     if(verbose) console.log("Setting up loop");
@@ -20,13 +33,16 @@ function graphSlice(){
     var i0max = _getNGHOSTS(1)-min;
     var i1max = _getNGHOSTS(2)-min;
     var i2max = _getNGHOSTS(3)-min;
+
     var i=0;
-    
+    var j=0;
+
     if(verbose) console.log("Looping");
 
     for(let n=0;n<=N_final;n++){
         
         if(n%100 == 0 || n==N_final) {
+            j++;
             if(verbose) console.log("Generating output for plotting", n);
             for(let i2=min;i2<i2max;i2++){
                 for(let i1=min;i1<i1max;i1++){
@@ -36,23 +52,22 @@ function graphSlice(){
                         xCart[0] = _getCart(i0,i1,i2,1);
                         xCart[1] = _getCart(i0,i1,i2,2);
                         console.log(xCart[0], xCart[1], _getIDX4ptS(fx,idx), _getFxVal(_getIDX4ptS(fx,idx)));
-                        
-                        data.x[i] = xCart[0];
-                        data.y[i] = xCart[1];
-                        data.z[i] = _getFxVal(_getIDX4ptS(fx,idx));
-                        i++;
+                        //data.x[i] = xCart[0];
+                        //data.y[i] = xCart[1];
+                        //data.y[i] = _getCF(_getIDX4ptS(fx,idx));
+                        //i++;
                     }
                 }
             }
             if(verbose) console.log("Done generating output for plotting", n);
-            updateMesh(data.z, data.x, data.y);
         }
+
         if(verbose) console.log("Taking step", n);
         _stepForward();
         if(verbose) console.log("Done taking step", n);
     }
 
-    
+    //Plotly.restyle("graph",update);
 };
 
 function runSim(){
